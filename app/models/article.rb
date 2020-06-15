380e92b14f48e63ad74f19fbb6e53a8ff42f9e18
abrormukimov <abrormm34@gmail.com>
2020-06-15 16:03:49 +0500

    Style articles listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-	validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+	validates :description, presence: true, length: { minimum: 10, maximum: 100000 }
 end
