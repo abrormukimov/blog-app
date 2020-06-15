446e3605f5f70dee0040e287dc6ec6e2752c815d
abrormukimov <abrormm34@gmail.com>
2020-06-15 16:48:40 +0500

    Create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_035729) do
+ActiveRecord::Schema.define(version: 2020_06_15_112526) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,4 +19,20 @@ ActiveRecord::Schema.define(version: 2020_06_12_035729) do
     t.datetime "updated_at"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "comments", "users"
 end
