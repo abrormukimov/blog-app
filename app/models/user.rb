64388dfe7e48c952beff04359206faf2ddfd1793
abrormukimov <abrormm34@gmail.com>
2020-06-16 11:57:57 +0500

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	has_many :articles 
+	has_many :articles
 	before_save { self.email = email.downcase }
 	validates :username, presence: true, uniqueness: { case_sensitive: false }, 
 			  length: { minimum: 3, maximum: 25 }
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 	validates :email, presence: true, length: { maximum: 105 },
 			  uniqueness: { case_sensitive: false }, 
 			  format: { with: VALID_EMAIL_REGEX }
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_032452) do
+ActiveRecord::Schema.define(version: 2020_06_16_065124) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_032452) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "users"
